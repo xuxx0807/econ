8f25fafd5236022bb8a6c5ef31c4799409844ba4
xuxx0807 <xruihong@gmail.com>
2019-01-31 11:17:00 -0500

    Create Econ_8307_Assignment_3.m

---
+++ Econ_8307_Assignment_3.m
@@ -0,0 +1,34 @@
+%Q1
+N=10;
+epsilon=1;
+delta=.1;
+beta=1;
+gamma=.1;
+z=1:N;
+transitMatrix=eye(N);
+alpha=zeros(N,1);
+phi=ones(N,1)/N;
+mu=zeros(N,1);
+
+for i=1:N
+	for j=1:N
+		alpha(i)=alpha(i)+max(0,beta-gamma*(z(j)-z(i))^2);
+	end
+end
+for i=1:N
+	for j=1:N
+		transitMatrix(i,j)=max(0,(beta-gamma*(z(j)-z(i))^2)/alpha(i))
+	end
+end
+
+mu=((1-delta)*transitMatrix.'-eye(N))\(-epsilon*phi)
+
+%Q3
+clear
+N=5;
+w=1;
+alpha=.7;
+beta=.95;
+E=1;
+lambda=.1;
+tau=0;
