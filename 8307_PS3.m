922b6a6173d8d6226cd0d1ae68d5a3998ddf407b
jonahcoste <46860109+jonahcoste@users.noreply.github.com>
2019-02-02 14:09:10 -0500

    update

--- 8307_PS3.m
+++ 8307_PS3.m
@@ -35,3 +35,29 @@ mustar = e*psi*inv(I-(1-delta)*T);
 
 mustar'
 
+%Question 3
+%Part 1
+N = 5;
+p = .8;
+w = 1;
+alpha = .7;
+beta = .95;
+lambda = .1;
+
+Z = zeros(1,N);
+nstar = zeros(N,1);
+for i =1:N
+    Z(i) = i;
+    nstar(i) = (w/(Z(i)*alpha))^(1/(alpha-1));
+end
+
+nstar
+
+T = zeros(N);
+T=T+(1-p)/(N-1)+eye(N)*(p-(1-p)/(N-1));
+I=eye(N);
+pi = zeros(N,1);
+for i =1:N
+     pi(i)=Z(i)*nstar(i)^alpha - w*nstar(i);
+ end
+V = inv(I-beta*(1-lambda)*T)*pi
