6ae7587141dc183ee47529bf8bf27f54166c830c
xuxx <xruihong@gmail.com>
2019-03-16 16:45:51 -0400

    Update ps7.m

--- ps7/ps7.m
+++ ps7/ps7.m
@@ -13,6 +13,11 @@ laborMarket = @(w) sum(mu.*(w/(theta.*z).^(1/(theta-1))),'all')-alpha/theta;
 guess=1;
 wage=fsolve(laborMarket,guess);
 
+muPath=zeros(2,200);
+nPath=zeros(2,200);
+wPath=zeros(1,200);
+yPath=zeros(1,200);
+muPath(:,1)=[45;0];
 for j=2:200
-	muTran(:,j)=E*psi+(1-delta)*TranMat'*muTran(:,j-1);
+	muPath(:,j)=E*psi+(1-delta)*TranMat'*muPath(:,j-1);
 end
